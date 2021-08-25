980907782b6ef83fb5e9e7b69de25fb1b3d41475
Troy Dorman <troydorman@gmail.com>
2021-08-25 10:42:14 -0600

    successfully filtering list based on filtered input value. done!

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const listItems = [];
 
 getData();
 
+filter.addEventListener("input", (e) => filterData(e.target.value));
+
 async function getData() {
   const response = await fetch("https://randomuser.me/api?results=50");
   const { results } = await response.json();
@@ -28,5 +30,15 @@ async function getData() {
     `;
 
     result.appendChild(li);
+  });
+}
+
+function filterData(searchTerm) {
+  listItems.forEach((item) => {
+    if (item.innerText.toLowerCase().includes(searchTerm.toLowerCase())) {
+      item.classList.remove("hide");
+    } else {
+      item.classList.add("hide");
+    }
   });
 }
